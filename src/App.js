fb9c634ace82b7c3e5c88dbaae794e26395b02d4
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-08-18 21:30:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Detail from "./pages/Detail/Detail";
+import Header from "./components/Home/Header/Header";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import Profile from "./pages/Profile/Profile";
 import React from "react";
 
 function App() {
-  return <div className="App">Initialize project</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/detail/:id" component={Detail} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="*" component={PageNotFound} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
