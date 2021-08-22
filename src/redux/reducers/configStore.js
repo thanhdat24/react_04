4069c4cc1bc540bbf965acbeebde005923429938
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-08-22 10:44:02 +0700

    Setup & Install redux-saga

--- src/redux/reducers/configStore.js
+++ src/redux/reducers/configStore.js
@@ -1,13 +1,23 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 
 import ToDoListReducer from "./ToDoListReducer";
+import createMiddlewareSaga from "redux-saga";
 import reduxThunk from "redux-thunk";
+import { rootSaga } from "../sagas/rootSaga";
+
+// middleware saga
+const middlewareSaga = createMiddlewareSaga();
 
 const rootReducer = combineReducers({
   // reducer khai báo tại đây
   ToDoListReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(reduxThunk, middlewareSaga)
+);
+// Call saga
+middlewareSaga.run(rootSaga);
 
 export default store;
